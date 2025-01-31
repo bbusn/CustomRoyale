390c751a60ea58fed7564a0eb445fd31c80c24fd
bbusn <benoit.busnardo@gmail.com>
2025-01-31 21:15:32 +0100

    Add basic Program.cs in CustomRoyale

--- CustomRoyale/Program.cs
+++ CustomRoyale/Program.cs
@@ -1,3 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.ReadKey();
+﻿using System;
+using System.Threading;
+
+namespace CustomRoyale
+{
+    public static class Program
+    {
+        private static void Main()
+        {
+            Console.Clear();
+            Console.Title = "CustomRoyale";
+            Console.ForegroundColor = ConsoleColor.Yellow;
+
+            Console.WriteLine("___________________________________________________________________________\n\n");
+            Console.WriteLine(
+                "                  __                                            __       \n" +
+                ".----.--.--.-----|  |_.-----.--------.  .----.-----.--.--.---.-|  .-----.\n" +
+                "|  __|  |  |__ --|   _|  _  |        |  |   _|  _  |  |  |  _  |  |  -__|\n" +
+                "|____|_____|_____|____|_____|__|__|__|  |__| |_____|___  |___._|__|_____| \n" +
+                "                                                   |_____|\n"
+            );
+
+            Console.WriteLine("Thanks to Incredible for orginal version of CR server.\nThanks to Zordon1337 for ZrdRoyale, a RetroRoyale Fork");
+            Console.WriteLine("Fork of ZrdRoyale by bbusn");
+            Console.WriteLine("___________________________________________________________________________\n\n");
+            Console.WriteLine("Press any key to shutdown the server....");
+                
+            Console.Read();
+            Shutdown();
+        }
+
+        public static void Shutdown()
+        {
+            Console.WriteLine("Shutting down server...");
+        }
+
+        public static void Exit()
+        {
+            Console.WriteLine("Shutting down server...");
+            Environment.Exit(0);
+        }
+    }
+}
